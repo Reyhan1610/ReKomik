c62478a97da805b3f0b55183d4837c415d2d813c
Reyhan1610 <alanareyhan1@gmail.com>
2023-07-14 15:14:59 +0700

    add dataset

--- data.py
+++ data.py
@@ -5,32 +5,32 @@ from preprocessing import preprocess
 
 def load_data():
     # Koneksi ke database
-    cnx = mysql.connector.connect(user='root', password='',
-                                  host='localhost',
-                                  database='ta1')
+    #cnx = mysql.connector.connect(user='root', password='',
+    #                             host='localhost',
+    #                              database='ta1')
     
     # Membaca file csv pertama
-    #data1 = pd.read_csv('data1.csv')
+    data1 = pd.read_csv('dataset/data1.csv')
     
     # Membaca file csv kedua
-    #data2 = pd.read_csv('data2.csv')
+    data2 = pd.read_csv('dataset/data2.csv')
 
     # Menggabungkan data dari kedua file csv
-    #data = pd.concat([data1, data2], ignore_index=True)
-
-    # Query untuk mengambil data dari tabel
-    query1 = "SELECT e.judul_elex, e.sinopsis_elex FROM elex e"
-    
-    # Membuat dataframe dari hasil query
-    data1 = pd.read_sql(query1, cnx)
-
-    # Query untuk mengambil data dari tabel
-    query2 = "SELECT w.judul_webtoon, w.sinopsis_webtoon FROM webtoon_id w"
-    
-    # Membuat dataframe dari hasil query
-    data2 = pd.read_sql(query2, cnx)
-
     data = pd.concat([data1, data2], ignore_index=True)
+
+    # Query untuk mengambil data dari tabel
+    #query1 = "SELECT e.judul_elex, e.sinopsis_elex FROM elex e"
+    
+    # Membuat dataframe dari hasil query
+    #data1 = pd.read_sql(query1, dt)
+
+    # Query untuk mengambil data dari tabel
+    #query2 = "SELECT w.judul_webtoon, w.sinopsis_webtoon FROM webtoon_id w"
+    
+    # Membuat dataframe dari hasil query
+    #data2 = pd.read_sql(query2, dt)
+
+    #data = pd.concat([data1, data2], ignore_index=True)
     
     # Menggabungkan judul dan sinopsis menjadi satu dokumen
     data = data.fillna('')
@@ -38,7 +38,7 @@ def load_data():
     data['dokumen'] = data['judul_elex'] + ' ' + data['sinopsis_elex'] + ' ' + data['judul_webtoon'] + ' ' + data['sinopsis_webtoon']
     data['dokumen'] = data['dokumen'].apply(preprocess)
     # Menutup koneksi ke database
-    cnx.close()
+    #cnx.close()
     
     return data
 
